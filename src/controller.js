530a3f672b84b769d43e57aca5afc364c4bf5517
Eduard-ua <61416842+Eduard-ua@users.noreply.github.com>
2021-04-04 18:18:22 +0300

    add origin link from master & complete all pages

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,4 @@
-async function profile(ctx) {
+async function home(ctx) {
   await ctx.render('index', {
     title: 'Home',
   });
@@ -10,15 +10,63 @@ async function signIn(ctx) {
   });
 }
 
-async function signIn(ctx) {
-  await ctx.render('sign_in_1', {
+async function signIn2(ctx) {
+  await ctx.render('sign_in_2', {
     title: 'Sign In',
   });
 }
 
+async function signIn3(ctx) {
+  await ctx.render('sign_in_3', {
+    title: 'Sign In',
+  });
+}
+
+async function signIn4(ctx) {
+  await ctx.render('sign_in_4', {
+    title: 'Sign In',
+  });
+}
+
+async function signUp(ctx) {
+  await ctx.render('sign_up_1', {
+    title: 'Sign Up',
+  });
+}
+
+async function signUp2(ctx) {
+  await ctx.render('sign_up_2-4', {
+    title: 'Sign Up',
+  });
+}
+
+async function profile(ctx) {
+  await ctx.render('my_profile', {
+    title: 'Profile',
+  });
+}
+
+async function search(ctx) {
+  await ctx.render('my_search', {
+    title: 'Search',
+  });
+}
+
+async function users(ctx) {
+  await ctx.render('users', {
+    title: 'Users',
+  });
+}
 
 module.exports = {
+  home,
   signIn,
+  signIn2,
+  signIn3,
+  signIn4,
+  signUp,
+  signUp2,
   profile,
-
+  search,
+  users,
 };
--- src/router.js
+++ src/router.js
@@ -4,9 +4,16 @@ const controllers = require('./controller');
 
 const router = new Router();
 
-router.get('home', controllers.profile);
+router.get('home', controllers.home);
 router.get('signIn', controllers.signIn);
-
+router.get('signIn2', controllers.signIn2);
+router.get('signIn3', controllers.signIn3);
+router.get('signIn4', controllers.signIn4);
+router.get('signUp', controllers.signUp);
+router.get('signUp2', controllers.signUp2);
+router.get('profile', controllers.profile);
+router.get('search', controllers.search);
+router.get('users', controllers.users);
 
 module.exports = {
   router,
